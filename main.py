9329a2fe790e3e6e7278f7ea48fd91e2e76f391c
LanceStemple <stemplel@msu.edu>
2023-12-17 20:53:32 -0500

    Most basic form of a single round of blackjack complete

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ BG = pygame.transform.scale(pygame.image.load("background.jpg"), (WIDTH, HEIGHT)
 FONT = pygame.font.SysFont("timesnewroman", 30)
 
 
-def draw(elapsed_time, deck_rect, flipped_deck, player_hand, dealer_hand):
+def draw(elapsed_time, deck_rect, flipped_deck, player_hand, dealer_hand, stay_button):
     WIN.blit(BG, (0, 0))
 
     time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
@@ -35,6 +35,9 @@ def draw(elapsed_time, deck_rect, flipped_deck, player_hand, dealer_hand):
         WIN.blit(dealer_value_text, (100, 100))
         WIN.blit(player_text, (100, 600))
         WIN.blit(player_value_text, (100, 650))
+        stay_text = FONT.render('Stay', True, 'white')
+        pygame.draw.rect(WIN, (180, 180, 180), stay_button)
+        WIN.blit(stay_text, (stay_button.x + 23, stay_button.y+10))
 
     pygame.display.update()
 
@@ -45,6 +48,8 @@ def main():
     clock = pygame.time.Clock()
     start_time = time.time()
     elapsed_time = 0
+
+    stay_button = pygame.Rect(650, 400, 100, 60)
 
     deck_image = pygame.image.load('deck.png').convert()
     deck_image = pygame.transform.scale(deck_image, (150, 250))
@@ -84,8 +89,20 @@ def main():
                             print("lose")
                             run = False
                             break
+                elif stay_button.collidepoint(x, y) and deck.is_flipped():
+                        deck.set_clickable(False)
+                        while(dealer_hand.get_value() < 17):
+                            dealer_hand.hit(deck)
+                        if(dealer_hand.get_value() >= player_hand.get_value() and dealer_hand.get_value() <= 21):
+                            print("lose")
+                            run = False
+                            break
+                        else:
+                            print("win")
+                            run = False
+                            break
 
-        draw(elapsed_time, deck_rect, deck.is_flipped(), player_hand, dealer_hand)
+        draw(elapsed_time, deck_rect, deck.is_flipped(), player_hand, dealer_hand, stay_button)
 
     pygame.quit()
 
